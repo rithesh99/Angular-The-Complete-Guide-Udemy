39a2786287487b8caba3f42073d3a12ab8687cdc
rithesh99 <ritheshdavid@gmail.com>
2021-12-19 08:49:17 +0530

    Component selector

--- a1-my-first-app/src/app/servers/servers.component.ts
+++ a1-my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // selector: '[app-servers]',  --attribute
+  // selector: '.app-servers',   --class
+  template: `
+  <h1>Servers component</h1>
+  <p>Inline template</p>
+  `,
+  styles: [`
+      h1 {
+        color: red;
+      }
+    `],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
