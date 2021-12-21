43884047edfd0414cdaf6c63f343557e78df726b
rithesh99 <ritheshdavid@gmail.com>
2021-12-21 08:59:46 +0530

    Header component

--- a2-second-app/src/app/header/header.component.ts
+++ a2-second-app/src/app/header/header.component.ts
@@ -3,9 +3,9 @@ import { Component } from "@angular/core"
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./']
+    styleUrls: []
 })
 
 export class HeaderComponent {
-
+    collapsed = true;
 }
