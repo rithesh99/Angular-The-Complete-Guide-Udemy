953e3656b76cec03b83ea75d3603f977fed262e1
rithesh99 <ritheshdavid@gmail.com>
2021-12-28 21:01:35 +0530

    Aliasing custom property

--- a3-third-app/src/app/app.component.ts
+++ a3-third-app/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
-  
- 
+  serverElements = [
+    {
+      type: "server",
+      name: "Testserver",
+      content: "Just a test!",
+    },
+    {
+      type: "blueprint",
+      name: "Testserver - 2",
+      content: "Just a test! - 2",
+    },
+  ];
 }
