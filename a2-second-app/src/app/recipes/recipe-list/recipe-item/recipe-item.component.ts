c67764709e954fa7ceb3ea817ae77b2f947bb97e
rithesh99 <ritheshdavid@gmail.com>
2022-01-10 21:59:49 +0530

    Passing data from child to parent

--- a2-second-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ a2-second-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe;
+  @Input() recipe: Recipe;
+  @Output() recipeSelectedEvent = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeClick() {
+    this.recipeSelectedEvent.emit();
   }
-
 }
