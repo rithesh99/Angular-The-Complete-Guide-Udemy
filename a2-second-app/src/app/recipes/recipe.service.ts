a3f216bafe085c053fe07e75333e5573ab7d4280
rithesh99 <ritheshdavid@gmail.com>
2022-02-12 13:04:33 +0530

    Added ingredients in recipe model

--- a2-second-app/src/app/recipes/recipe.service.ts
+++ a2-second-app/src/app/recipes/recipe.service.ts
@@ -1,25 +1,32 @@
-import { EventEmitter } from "@angular/core";
-import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe(
-          'A Test Recipe 1',
-          'This is simply a test',
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-        new Recipe(
-          '2nd test recipe',
-          'This is simply a test',
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-      ];
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe 1',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 399),
+        new Ingredient('French Fries', 89)
+      ]
+    ),
+    new Recipe(
+      '2nd test recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Burger', 99),
+        new Ingredient('Pizza', 199)
+      ]
+    ),
+  ];
 
-    getRecipes(){
-        // return this.recipes.slice();
-        return this.recipes;
-    }
-
-
-};
+  getRecipes() {
+    // return this.recipes.slice();
+    return this.recipes;
+  }
+}
