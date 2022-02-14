0f8726d9c7352a9ba45fc332d3aa75d8bf3f5ac5
rithesh99 <ritheshdavid@gmail.com>
2022-02-14 10:06:15 +0530

    Added routes and routerlink

--- a6-sixth-app/src/app/app.module.ts
+++ a6-sixth-app/src/app/app.module.ts
@@ -1,17 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
+const appRoutes: Routes = [
+  { 
+    path: "", 
+    component: HomeComponent 
+  },
+  { 
+    path: "servers", 
+    component: ServersComponent 
+  },
+  { 
+    path: "users", 
+    component: UsersComponent 
+  },
 
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,13 +35,14 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
