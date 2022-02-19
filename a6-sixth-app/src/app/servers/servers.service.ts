c2f49370b2f061026d4720291a68f09d270a6dfc
rithesh99 <ritheshdavid@gmail.com>
2022-02-19 23:46:14 +0530

    Query and fragment

--- a6-sixth-app/src/app/servers/servers.service.ts
+++ a6-sixth-app/src/app/servers/servers.service.ts
@@ -24,7 +24,7 @@ export class ServersService {
   getServer(id: number) {
     const server = this.servers.find(
       (s) => {
-        return s.id === id;
+        return s.id == id;
       }
     );
     return server;
