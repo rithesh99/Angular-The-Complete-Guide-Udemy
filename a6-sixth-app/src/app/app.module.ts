169cb3be785a188aa8b3c41bf5f000d419684e33
rithesh99 <ritheshdavid@gmail.com>
2022-02-20 09:04:23 +0530

    Nested routes

--- a6-sixth-app/src/app/app.module.ts
+++ a6-sixth-app/src/app/app.module.ts
@@ -13,31 +13,34 @@ import { ServersService } from "./servers/servers.service";
 import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-  { 
-    path: "", 
-    component: HomeComponent 
+  {
+    path: "",
+    component: HomeComponent,
   },
-  { 
-    path: "servers", 
-    component: ServersComponent 
+  {
+    path: "servers",
+    component: ServersComponent,
+    children: [
+      {
+        path: ":id/edit",
+        component: EditServerComponent,
+      },
+      {
+        path: ":id",
+        component: ServerComponent,
+      },
+    ],
   },
-  { 
-    path: "servers/:id/edit", 
-    component: EditServerComponent 
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [
+      {
+        path: ":id/:name",
+        component: UserComponent,
+      },
+    ],
   },
-  { 
-    path: "servers/:id", 
-    component: ServerComponent 
-  },
-  { 
-    path: "users", 
-    component: UsersComponent 
-  },
-  { 
-    path: "users/:id/:name", 
-    component: UserComponent 
-  },
-
 ];
 @NgModule({
   declarations: [
@@ -49,11 +52,7 @@ const appRoutes: Routes = [
     EditServerComponent,
     ServerComponent,
   ],
-  imports: [
-    BrowserModule, 
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
