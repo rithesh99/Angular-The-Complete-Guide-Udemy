8c0664fd564b4123b8ad8988769b1cfaa9a91f33
rithesh99 <ritheshdavid@gmail.com>
2022-02-22 00:10:19 +0530

    Auth guard canActivate

--- a6-sixth-app/src/app/app-routing.module.ts
+++ a6-sixth-app/src/app/app-routing.module.ts
@@ -4,44 +4,54 @@ import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NgModule } from "@angular/core";
+import { AuthGuard } from "./auth-guard.service";
 
-export const appRoutes: Routes = [
-    {
-      path: "",
-      component: HomeComponent,
-    },
-    {
-      path: "servers",
-      component: ServersComponent,
-      children: [
-        {
-          path: ":id/edit",
-          component: EditServerComponent,
-        },
-        {
-          path: ":id",
-          component: ServerComponent,
-        },
-      ],
-    },
-    {
-      path: "users",
-      component: UsersComponent,
-      children: [
-        {
-          path: ":id/:name",
-          component: UserComponent,
-        },
-      ],
-    },
-    {
-      path: "page-not-found",
-      component: PageNotFoundComponent,
-    },
-    {
-      path: "**",
-      redirectTo: "/page-not-found",
-    }
-  ];
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "servers",
+    canActivate: [AuthGuard],
+    component: ServersComponent,
+    children: [
+      {
+        path: ":id/edit",
+        component: EditServerComponent,
+      },
+      {
+        path: ":id",
+        component: ServerComponent,
+      },
+    ],
+  },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [
+      {
+        path: ":id/:name",
+        component: UserComponent,
+      },
+    ],
+  },
+  {
+    path: "page-not-found",
+    component: PageNotFoundComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/page-not-found",
+  },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+  })
+
+export class AppRoutingModule {}
--- a6-sixth-app/src/app/app.module.ts
+++ a6-sixth-app/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { appRoutes } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AuthGuard } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 
 @NgModule({
   declarations: [
@@ -25,8 +26,8 @@ import { appRoutes } from "./app-routing.module";
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
