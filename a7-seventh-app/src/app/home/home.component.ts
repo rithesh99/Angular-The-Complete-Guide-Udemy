258ce25b3203ae0ebfee6503669127a9e849cb50
rithesh99 <ritheshdavid@gmail.com>
2022-02-26 00:24:22 +0530

    Custom observable

--- a7-seventh-app/src/app/home/home.component.ts
+++ a7-seventh-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  private firstObsSubscription: Subscription;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    // const customIntervalObservable = new Observable((observer: any) => {
+    const customIntervalObservable = Observable.create((observer: any) => {
+      let count: number = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+        // observer.error();
+        // observer.complete();
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (count: any) => {
+        console.log(count);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
