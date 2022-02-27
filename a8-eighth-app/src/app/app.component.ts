0b98b6907b29132b9fdc529c0b50b3de6db12973
rithesh99 <ritheshdavid@gmail.com>
2022-02-27 21:06:52 +0530

    Resetting form

--- a8-eighth-app/src/app/app.component.ts
+++ a8-eighth-app/src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  @ViewChild('f') formData: NgForm;
-  answer: String = '';
-  genders: Array<string> = ['Male', 'Female', 'Others'];
+  @ViewChild("f") formData: NgForm;
+  answer: String = "";
+  genders: Array<string> = ["Male", "Female", "Others"];
 
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
     this.formData.form.patchValue({
       userData: {
-        username: suggestedName
-      }
+        username: suggestedName,
+      },
     });
   }
 
@@ -25,6 +25,10 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.formData.value)
+    console.log(this.formData.value);
+  }
+
+  resetForm() {
+    this.formData.reset();
   }
 }
