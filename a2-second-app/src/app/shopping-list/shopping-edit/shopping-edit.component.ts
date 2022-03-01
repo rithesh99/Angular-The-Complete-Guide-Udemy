c10b9b581f4ded494ce7d87633e7d7678d333e09
rithesh99 <ritheshdavid@gmail.com>
2022-03-01 23:06:46 +0530

    Added Template driven form

--- a2-second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ a2-second-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,6 +4,7 @@ import {
   ElementRef,
   ViewChild
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -14,18 +15,19 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  // @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  // @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(formData: NgForm) {
+    // const ingName = this.nameInputRef.nativeElement.value;
+    // const ingAmount = this.amountInputRef.nativeElement.value;
+    console.log(formData.value);
+    const newIngredient = new Ingredient(formData.value.name, formData.value.amount);
     this.slService.addIngredient(newIngredient);
   }
 
