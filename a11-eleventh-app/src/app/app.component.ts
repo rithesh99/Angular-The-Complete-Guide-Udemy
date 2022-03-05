cdd73a43ce2a5be7a5d6b9cfa3e2a769ef04915e
rithesh99 <ritheshdavid@gmail.com>
2022-03-05 12:57:17 +0530

    POST request

--- a11-eleventh-app/src/app/app.component.ts
+++ a11-eleventh-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -15,6 +15,14 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.http
+      .post(
+        "https://ng-udemy-4e040-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
     console.log(postData);
   }
 
