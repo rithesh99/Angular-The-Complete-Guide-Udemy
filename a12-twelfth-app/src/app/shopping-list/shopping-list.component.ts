36e3c132e8ea9c9b172c01a26c296a1b74ad9f43
rithesh99 <ritheshdavid@gmail.com>
2022-03-15 09:28:19 +0530

    Selecting state

--- a12-twelfth-app/src/app/shopping-list/shopping-list.component.ts
+++ a12-twelfth-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
@@ -11,24 +12,30 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-  subscription: Subscription;
+  private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.subscription = this.store.select('shoppingList').subscribe((data) => {
+      this.ingredients = data.ingredients;
+    });
+    // this.ingredients = this.slService.getIngredients();
+    // this.subscription = this.slService.ingredientsChanged.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  onEditItem(index: number){
+  onEditItem(index: number) {
     this.slService.startedEditing.next(index);
   }
 }
