e9814602f4a5fa34801120477b1880a61340f287
rithesh99 <ritheshdavid@gmail.com>
2022-03-17 23:00:30 +0530

    Auth reducers and actions

--- a12-twelfth-app/src/app/auth/store/auth.reducer.ts
+++ a12-twelfth-app/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from './../user.model';
+import { AuthActions, LOGIN, LOGOUT } from './auth.actions';
 export interface AuthState {
   user: User;
 }
@@ -7,6 +8,25 @@ const initialState: AuthState = {
   user: null,
 };
 
-export function authReducer(state = initialState, action) {
-    return state;
+export function authReducer(state = initialState, action: AuthActions) {
+  switch (action.type) {
+    case LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user: user,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
 }
