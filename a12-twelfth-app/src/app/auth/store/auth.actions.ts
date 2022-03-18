fca91ad6a0a59c695256a387db7ebc3d0abb83bd
rithesh99 <ritheshdavid@gmail.com>
2022-03-18 09:16:48 +0530

    Modified actions name

--- a12-twelfth-app/src/app/auth/store/auth.actions.ts
+++ a12-twelfth-app/src/app/auth/store/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] Logout';
+export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
   readonly type = LOGIN;
